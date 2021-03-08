f33369c84416bde1229ca551e17ef98d2e7008d5
Ana <ana@altonotch.com>
2021-03-08 23:18:48 +0200

    Update celery runner to restart on code change
    Signed-off-by: Ana <ana@altonotch.com>

--- src/scheduler_runner.sh
+++ src/scheduler_runner.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sleep 10
 
-until pg_isready -h ${DB_HOST} -p 5432 -U ${DB_USER}:${DB_PASSWORD} -d ${DB_NAME}
+until pg_isready -h "${DB_HOST}" -p 5432 -U "${DB_USER}:${DB_PASSWORD}" -d "${DB_NAME}"
 do
   echo "Waiting for postgres 5 sec ... "
   sleep 5;
@@ -10,6 +10,7 @@ done
 # Django check
 python manage.py check
 
-# Start celery worker via watchdog
-# Start with beat scheduler as well; ok in development, not in production!
-celery -A backend worker -l info
+exec watchmedo auto-restart -d . -p "*.py" --recursive -- \
+  dockerize -wait http://api:8000/health/check/ \
+  celery -A backend worker -l info -B --concurrency 4
+
